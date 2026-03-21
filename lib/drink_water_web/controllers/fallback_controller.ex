602f8f9a8c7634cf720da8b03739c1d5f57661b1
eduardokohn <eduardokohn15@gmail.com>
2026-03-21 11:03:53 -0300

    refactor: FallbackController uses ErrorCatalog instead of ProblemDetail

--- lib/drink_water_web/controllers/fallback_controller.ex
+++ lib/drink_water_web/controllers/fallback_controller.ex
@@ -1,18 +1,18 @@
 defmodule DrinkWaterWeb.FallbackController do
   @moduledoc """
   Translates controller action results into valid `Plug.Conn` responses
-  using RFC 7807 Problem Details.
+  using RFC 9457 Problem Details.
   """
   use DrinkWaterWeb, :controller
 
-  alias DrinkWaterWeb.ProblemDetail
+  alias DrinkWaterWeb.ErrorCatalog
 
   def call(conn, {:error, :not_found, resource}) when is_atom(resource) do
     conn
     |> put_problem_content_type()
     |> put_status(:not_found)
     |> put_view(json: DrinkWaterWeb.ErrorJSON)
-    |> render(:error, problem: ProblemDetail.build(conn, :not_found, resource))
+    |> render(:error, problem: ErrorCatalog.build(conn, :not_found, resource))
   end
 
   def call(conn, {:error, :conflict, resource}) when is_atom(resource) do
@@ -20,7 +20,7 @@ defmodule DrinkWaterWeb.FallbackController do
     |> put_problem_content_type()
     |> put_status(:conflict)
     |> put_view(json: DrinkWaterWeb.ErrorJSON)
-    |> render(:error, problem: ProblemDetail.build(conn, :conflict, resource))
+    |> render(:error, problem: ErrorCatalog.build(conn, :conflict, resource))
   end
 
   def call(conn, {:error, :bad_request}) do
@@ -28,7 +28,7 @@ defmodule DrinkWaterWeb.FallbackController do
     |> put_problem_content_type()
     |> put_status(:bad_request)
     |> put_view(json: DrinkWaterWeb.ErrorJSON)
-    |> render(:error, problem: ProblemDetail.build(conn, :bad_request))
+    |> render(:error, problem: ErrorCatalog.build(conn, :bad_request))
   end
 
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
@@ -36,7 +36,7 @@ defmodule DrinkWaterWeb.FallbackController do
     |> put_problem_content_type()
     |> put_status(:unprocessable_entity)
     |> put_view(json: DrinkWaterWeb.ErrorJSON)
-    |> render(:error, problem: ProblemDetail.from_changeset(conn, changeset))
+    |> render(:error, problem: ErrorCatalog.from_changeset(conn, changeset))
   end
 
   defp put_problem_content_type(conn) do
